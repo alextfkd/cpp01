8b7f0f07d146b253995465af4962d0a8a07f8dad
Takahiro Katsumata <alex.tfkd@gmail.com>
2025-12-18 02:36:20 +0000

    コピー演算子・コピー代入演算子をPublicにした

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,29 +6,29 @@
 /*   By: tkatsuma <tkatsuma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 10:37:40 by tkatsuma          #+#    #+#             */
-/*   Updated: 2025/12/16 12:53:03 by tkatsuma         ###   ########.fr       */
+/*   Updated: 2025/12/18 02:26:43 by tkatsuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <string>
 #ifndef ZOMBIE_H
 #define ZOMBIE_H
+#include <iostream>
+#include <string>
 
 class Zombie {
  public:
   void announce(void) const;
-  void SetName(const std::string &name);
-  Zombie();
+  void SetName(const std::string& name);
+  Zombie(const std::string& name);
+  Zombie(const Zombie& other);
+  Zombie& operator=(const Zombie& other);
   ~Zombie();
 
  private:
   std::string name;  // NO LINT
-  Zombie(const Zombie &other);
-  Zombie &operator=(const Zombie &other);
 };
 
-Zombie *newZombie(std::string name);  // NO LINT
+Zombie* newZombie(std::string name);  // NO LINT
 void    randomChump(std::string name);
 
 #endif
