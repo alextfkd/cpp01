1c0cc25c6eaca68bf6ee95cdea95a682bbb45ba6
Takahiro Katsumata <alex.tfkd@gmail.com>
2025-12-18 12:08:02 +0000

    ex 05 finished

---
+++ ex05/Harl.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkatsuma <tkatsuma@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/18 11:30:20 by tkatsuma          #+#    #+#             */
+/*   Updated: 2025/12/18 12:06:43 by tkatsuma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+void Harl::debug(void) { std::cout << this->kDebugMsg << std::endl; }
+void Harl::info(void) { std::cout << this->kInfoMsg << std::endl; }
+void Harl::warning(void) { std::cout << this->kWarningMsg << std::endl; }
+void Harl::error(void) { std::cout << this->kErrorMsg << std::endl; }
+
+Harl::Harl(const Harl& other) { (void)other; }
+Harl& Harl::operator=(const Harl& other) {
+  (void)other;
+  return *this;
+}
+
+Harl::Harl()
+    : kDebugMsg(
+          "I love having extra bacon for my "
+          "7XL-double-cheese-triple-pickle-specialketchup burger. I really "
+          "do!"),
+      kInfoMsg(
+          "I cannot believe adding extra bacon costs more money. You didn’t "
+          "put enough bacon in my burger! If you did, I wouldn’t be asking "
+          "for more!"),
+      kWarningMsg(
+          "I think I deserve to have some extra bacon for free. I’ve "
+          "been coming for years, whereas you started working here just "
+          "last month."),
+      kErrorMsg("This is unacceptable! I want to speak to the manager now.") {}
+
+Harl::~Harl() {}
+
+void Harl::complain(std::string level) {  // NOLINT
+  std::string levels[Harl::kNumLevels] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+  void (Harl::*says[Harl::kNumLevels])(void) = {
+      &Harl::debug,
+      &Harl::info,
+      &Harl::warning,
+      &Harl::error,
+  };
+  for (int idx = 0; idx < Harl::kNumLevels; idx++) {
+    if (levels[idx] == level) {
+      (this->*says[idx])();
+    }
+  }
+}
---
+++ ex05/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkatsuma <tkatsuma@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/18 11:55:55 by tkatsuma          #+#    #+#             */
+/*   Updated: 2025/12/18 12:03:05 by tkatsuma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main(int argc, char* argv[]) {
+  std::string kArgError =
+      "Error: invalid arguments. Use DEBUG, INFO, WARNING or ERROR "
+      "instead.";
+  if (argc != 2) {
+    std::cout << kArgError << std::endl;
+    return (1);
+  }
+  Harl        harl;
+  std::string level = argv[1];
+  harl.complain(level);
+  return (0);
+}
