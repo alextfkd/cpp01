b632e4e7dc7431cf6fbef195c344124ecd52f26e
Takahiro Katsumata <alex.tfkd@gmail.com>
2025-12-18 03:10:45 +0000

    set constructors as public

--- ex01/Zombie.hpp
+++ ex01/Zombie.hpp
@@ -6,28 +6,29 @@
 /*   By: tkatsuma <tkatsuma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 10:37:40 by tkatsuma          #+#    #+#             */
-/*   Updated: 2025/12/16 13:59:07 by tkatsuma         ###   ########.fr       */
+/*   Updated: 2025/12/18 03:08:16 by tkatsuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <string>
 #ifndef ZOMBIE_H
 #define ZOMBIE_H
+#include <iostream>
+#include <string>
 
 class Zombie {
  public:
   void announce(void) const;
-  void SetName(const std::string &name);
+  void SetName(const std::string& name);
   Zombie();
+  Zombie(const std::string& name);
+  Zombie(const Zombie& other);
+  Zombie& operator=(const Zombie& other);
   ~Zombie();
 
  private:
   std::string name;  // NO LINT
-  Zombie(const Zombie &other);
-  Zombie &operator=(const Zombie &other);
 };
 
-Zombie *zombieHorde(int N, std::string name);  // NOLINT
+Zombie* zombieHorde(int N, std::string name);  // NOLINT
 
 #endif
