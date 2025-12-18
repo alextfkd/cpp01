137b5808b0c87a1bd74bec25232741d69643c7cf
Takahiro Katsumata <alex.tfkd@gmail.com>
2025-12-18 03:31:25 +0000

    コンストラクタの整理および、getTypeの右Const追加。

--- ex03/Weapon.cpp
+++ ex03/Weapon.cpp
@@ -6,26 +6,24 @@
 /*   By: tkatsuma <tkatsuma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 20:27:20 by tkatsuma          #+#    #+#             */
-/*   Updated: 2025/12/17 00:55:20 by tkatsuma         ###   ########.fr       */
+/*   Updated: 2025/12/18 03:30:38 by tkatsuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Weapon.hpp"
 
-std::string type;  // NOLINT
-
 Weapon::Weapon(const std::string& type) { this->setType(type); }
 
 Weapon& Weapon::operator=(const Weapon& other) {
-  this->type = other.type;
+  if (this != &other) {
+    this->type = other.type;
+  }
   return *this;
 }
 
-Weapon::Weapon() {}
-
 Weapon::~Weapon() {}
 
-const std::string& Weapon::getType(void) {
+const std::string& Weapon::getType(void) const {
   const std::string& ref_type = this->type;
   return (ref_type);
 }
--- ex03/Weapon.hpp
+++ ex03/Weapon.hpp
@@ -6,25 +6,25 @@
 /*   By: tkatsuma <tkatsuma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 20:04:39 by tkatsuma          #+#    #+#             */
-/*   Updated: 2025/12/17 01:25:17 by tkatsuma         ###   ########.fr       */
+/*   Updated: 2025/12/18 03:30:45 by tkatsuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string>
-
 #ifndef WEAPON_H
 #define WEAPON_H
+#include <string>
 
 class Weapon {
  private:
   std::string type;  // NOLINT
 
  public:
-  Weapon();
   Weapon(const std::string& type);
+  Weapon(const Weapon& other);
   Weapon& operator=(const Weapon& other);
   ~Weapon();
-  const std::string& getType(void);
+
+  const std::string& getType(void) const;
   void               setType(const std::string& type);
 };
 
