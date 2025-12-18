1de535c3bbd628e9445a01797dea8a8c856bd8fd
Takahiro Katsumata <alex.tfkd@gmail.com>
2025-12-18 03:09:41 +0000

    lint

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,16 +6,16 @@
 /*   By: tkatsuma <tkatsuma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 12:31:35 by tkatsuma          #+#    #+#             */
-/*   Updated: 2025/12/16 19:34:10 by tkatsuma         ###   ########.fr       */
+/*   Updated: 2025/12/18 03:08:33 by tkatsuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   (void)argc;
   (void)argv;
-  Zombie *zombies = zombieHorde(4, "ugougo");
+  Zombie* zombies = zombieHorde(4, "ugougo");
   int     idx     = 0;
   while (idx < 4) {
     std::cout << "index: " << idx << std::endl;
