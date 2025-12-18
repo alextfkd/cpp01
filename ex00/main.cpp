e2f3e45e69260d2f9f3e633665eadba29c3a7a0e
Takahiro Katsumata <alex.tfkd@gmail.com>
2025-12-18 02:36:44 +0000

    可読性向上、主に名前の部分

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,19 +6,19 @@
 /*   By: tkatsuma <tkatsuma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 12:31:35 by tkatsuma          #+#    #+#             */
-/*   Updated: 2025/12/16 12:55:02 by tkatsuma         ###   ########.fr       */
+/*   Updated: 2025/12/18 02:17:55 by tkatsuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   (void)argc;
   (void)argv;
-  Zombie *z0 = newZombie("z0");
-  Zombie *z1 = newZombie("z1");
-  delete z0;
-  delete z1;
-  randomChump("r0");
+  Zombie* zombie_0 = newZombie("zombie_0");
+  Zombie* zombie_1 = newZombie("zombie_1");
+  delete zombie_0;
+  delete zombie_1;
+  randomChump("random zombie 0");
   return (0);
 }
