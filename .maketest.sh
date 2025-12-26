8cbd17264d5749b5de027e992134e25dfbe5562f
Takahiro Katsumata <alex.tfkd@gmail.com>
2025-12-26 15:56:32 +0000

    maker

---
+++ .maketest.sh
@@ -0,0 +1,35 @@
+cd ./ex00
+make cpplint
+make
+make fclean
+cd ..
+cd ./ex01
+make cpplint
+make
+make fclean
+cd ..
+cd ./ex02
+make cpplint
+make
+make fclean
+cd ..
+cd ./ex03
+make cpplint
+make
+make fclean
+cd ..
+cd ./ex04
+make cpplint
+make
+make fclean
+cd ..
+cd ./ex05
+make cpplint
+make
+make fclean
+cd ..
+cd ./ex06
+make cpplint
+make
+make fclean
+cd ..
