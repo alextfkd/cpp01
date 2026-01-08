955124eaa2cfd3d4795c18349028dc7dc9f25cd2
tkatsuma <tkatsuma@student.42.fr>
2026-01-08 21:58:43 +0900

    rm cpplint

--- .maketest.sh
+++ .maketest.sh
@@ -1,35 +1,28 @@
 cd ./ex00
-make cpplint
 make
 make fclean
 cd ..
 cd ./ex01
-make cpplint
 make
 make fclean
 cd ..
 cd ./ex02
-make cpplint
 make
 make fclean
 cd ..
 cd ./ex03
-make cpplint
 make
 make fclean
 cd ..
 cd ./ex04
-make cpplint
 make
 make fclean
 cd ..
 cd ./ex05
-make cpplint
 make
 make fclean
 cd ..
 cd ./ex06
-make cpplint
 make
 make fclean
 cd ..
